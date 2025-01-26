87da3e2c0113d22dfeeb31cb13722aa2a13eae33
Vika1402 <vikas.kumar123@ssipmt.com>
2025-01-26 16:00:00 +0530

    changed server url

--- src/utility/axiosSetup.js
+++ src/utility/axiosSetup.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://food-delivery-backend-intern-1.onrender.com/",
+  baseURL: "https://delivery-backend-intern.onrender.com/",
   headers: {
     "Content-Type": "application/json",
   },
